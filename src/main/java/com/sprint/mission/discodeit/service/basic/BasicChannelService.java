2207416d3377f671d4964ce6ff20ed31e86a882b
Linpield <top09100620@gmail.com>
2026-03-06 18:29:17 +0900

    msg : BasicChannelService의 주석 추가, 여백 조정

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicChannelService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicChannelService.java
@@ -66,7 +66,7 @@ public class BasicChannelService implements ChannelService {
     @Override
     public void readChannel(String channelId) {
 
-
+        //채널 가져오기
         Channel channel = channelRepository.getChannel(channelId);
 
         //채널 여부 체크
@@ -84,6 +84,7 @@ public class BasicChannelService implements ChannelService {
     @Override
     public void readAllChannel() {
 
+        //채널 가져오기
         List<Channel> channels = channelRepository.getAllChannel();
 
         //리스트 null 체크
@@ -92,6 +93,7 @@ public class BasicChannelService implements ChannelService {
             return;
         }
 
+        //출력
         channels.stream()
                 .sorted(Channel::compareTo)
                 .forEach(System.out::println);
@@ -109,7 +111,7 @@ public class BasicChannelService implements ChannelService {
             System.out.println("존재하지 않는 채널입니다.");
             return;
         }
-
+        //출력용 예전 닉네임
         String oldName = channel.getChannelName();
 
         //TODO : 비밀번호 체크 로직 넣기. 인터페이스 및 이전 코드들 약간씩 수정 필요
@@ -153,14 +155,6 @@ public class BasicChannelService implements ChannelService {
 
         //TODO : 비밀번호 체크 로직 넣기. 인터페이스 및 이전 코드들 약간씩 수정 필요
 
-//        User user = userRepository.getUser(channel.getOwnerId());
-//
-//        if(!user.checkSamePassword(password)){
-//
-//            System.out.println("비밀번호가 일치하지 않습니다.");
-//            return;
-//
-//        }
 
 
         //채널 멤버인지 체크
@@ -261,6 +255,9 @@ public class BasicChannelService implements ChannelService {
             return;
         }
 
+
+        //TODO : 비밀번호 체크 로직 넣기. 인터페이스 및 이전 코드들 약간씩 수정 필요. owner or member 둘중 아무나 맞으면 통과
+
         channel.removeMember(memberId);
 
         //삭제한 멤버가 채널장이었을때
@@ -311,68 +308,40 @@ public class BasicChannelService implements ChannelService {
             return;
         }
 
-
         //TODO : 비밀번호 체크 로직 넣기. 인터페이스 및 이전 코드들 약간씩 수정 필요
 
-//        User user = userRepository.getUser(channel.getOwnerId());
-//
-//        if(!user.checkSamePassword(password)){
-//
-//            System.out.println("비밀번호가 일치하지 않습니다.");
-//            return;
-//
-//        }
 
 
         if(!channelRepository.deleteChannel(channelId)){
             System.out.println("채널 삭제중 문제가 발생했습니다.");
             return;
-
-
         }
 
         //채널 삭제시 메시지 전부 삭제
         messageRepository.channelsMessagedelete(channelId);
 
-
-
-
-
-
-
-
+        //메시지 출력
         System.out.println(channel.getChannelName() + " 채널 삭제 완료!");
 
     }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //안쓰는 메서드
+    //BasicService 에서는 사용 안함
 
     @Override
     public boolean isExistChannel(String channelId) {
+        //채널 가져오기
+        Channel channel = channelRepository.getChannel(channelId);
+        return channel != null;
 
-        return false;
     }
 
     @Override
     public boolean isChannelsMember(String channelId, String memberId) {
-
-        return false;
+        //채널 가져오기
+        Channel channel = channelRepository.getChannel(channelId);
+        //채널이 없거나 멤버가 없으면 false
+        return channel != null && channel.getMembers().contains(memberId);
     }
 }
