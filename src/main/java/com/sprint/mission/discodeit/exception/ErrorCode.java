cc4ae1bd3ea5a6e64c3a0b2a7344fa42fd314a71
Linpield <top09100620@gmail.com>
2026-05-06 13:50:17 +0900

    fix : 예외 처리관련 오류 수정

--- src/main/java/com/sprint/mission/discodeit/exception/ErrorCode.java
+++ src/main/java/com/sprint/mission/discodeit/exception/ErrorCode.java
@@ -1,19 +1,18 @@
 package com.sprint.mission.discodeit.exception;
 
+import lombok.Getter;
+
+@Getter
 public enum ErrorCode {
 
 
   USER_NOT_FOUND,
   DUPLICATE_USER,
   CHANNEL_NOT_FOUND,
-  PRIVATE_CHANNEL_UPDATE
+  PRIVATE_CHANNEL_UPDATE;
 
 
   private String message;
 
-
-  public String getMessage() {
-    return message;
-  }
 
 }
