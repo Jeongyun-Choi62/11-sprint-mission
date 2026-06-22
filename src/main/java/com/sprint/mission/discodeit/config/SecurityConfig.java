7473b2f480dd1758a8182d45deb61ae69a358165
Linpield <top09100620@gmail.com>
2026-06-22 16:05:37 +0900

    feat: UserDetails, DetailsService 구현

--- src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
@@ -21,6 +21,9 @@ public class SecurityConfig {
         .csrf(csrf -> csrf
             .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
             .csrfTokenRequestHandler(new SpaCsrfTokenRequestHandler())
+        )
+        .formLogin(login -> login
+            .loginProcessingUrl("/api/auth/login")
         );
 
     return http.build();
